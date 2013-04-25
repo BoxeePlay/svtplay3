cfb38623f93d32a85f0d0835ca1dbee6313a5edf
Andreas Pehrson <pehrson@comoyo.com>
2013-04-25 23:19:54 +0200

    Prepare for initial release

--- tv.boxeeplay.svtplay3/start.py
+++ tv.boxeeplay.svtplay3/start.py
@@ -5,6 +5,6 @@ here we can do some pre launch processing if we wish.
 checking authentication or pre-loading content, anything we need.
 '''
 logger.EnablePlus(logger.Level.ERROR)
-logger.EnablePlus(logger.Level.DEBUG)
+#logger.EnablePlus(logger.Level.DEBUG)
 
 mc.ActivateWindow(14000)
