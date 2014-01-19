9db347efee3a804598f6f2b7e56b1c21012bc18c
Andreas Pehrson <pehrson@comoyo.com>
2014-01-19 18:37:56 +0100

    Propagate error during async operation properly

--- tv.boxeeplay.svtplay3/async_task.py
+++ tv.boxeeplay.svtplay3/async_task.py
@@ -10,19 +10,20 @@ class AsyncTask(threading.Thread):
         self.kwargs = kwargs
         self.setDaemon(True)
         self.returned = None
-        self.error = None
+        self.exc_info = None
 
     def run(self):
         try:
             self.returned = self.target(*self.args, **self.kwargs)
-        except Exception, e:
-            self.error = e
+        except Exception:
+            import sys
+            self.exc_info= sys.exc_info()
 
     def get_result(self):
         if self.isAlive():
             raise RuntimeError("Not finished yet")
-        if self.error is not None:
-            raise AsyncError("Error occurred while doing background work", self.error)
+        if self.exc_info:
+            raise self.exc_info[1], None, self.exc_info[2]
         return self.returned
 
 class AsyncError(Exception):
